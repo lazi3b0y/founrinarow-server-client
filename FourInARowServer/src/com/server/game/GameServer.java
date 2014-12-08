c2f5ee6e8606111e0ea70b0de742d5a0977ac2ee
Simon <Simon@LZBY>
2014-12-08 21:06:41 +0100

    Fixing with the try/catches.

--- FourInARowServer/src/com/server/game/GameServer.java
+++ FourInARowServer/src/com/server/game/GameServer.java
@@ -4,16 +4,44 @@ import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.*;
 
+import javax.swing.JOptionPane;
+
 import com.interf.fourinarow.*;
 
 public class GameServer {
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException {
-    	Registry registry = LocateRegistry.createRegistry(Constant.RMI_PORT);
+    public static void main(String[] args) {
+    	Registry registry = null;
+		try {
+			registry = LocateRegistry.createRegistry(Constant.RMI_PORT);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Exception Error.",JOptionPane.INFORMATION_MESSAGE);
+			System.out.println("Exception encountered. Terminating server.");
+			System.exit(0);
+		}
     	System.out.println("Registry instansiated.");
-    	ServerCom serverCom = new ServerCom();
+    	ServerCom serverCom = null;
+		try {
+			serverCom = new ServerCom();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Exception Error.",JOptionPane.INFORMATION_MESSAGE);
+			System.out.println("Exception encountered. Terminating server.");
+			System.exit(0);
+		}
     	System.out.println("ServerCom instansiated.");
-    	registry.bind(Constant.SERVERCOM_ID, serverCom);
-    	System.out.println("ServerCom bound to the ID: " + Constant.SERVERCOM_ID);
+    	try {
+			registry.bind(Constant.SERVERCOM_ID, serverCom);
+		} catch (RemoteException | AlreadyBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Exception Error.",JOptionPane.INFORMATION_MESSAGE);
+			System.out.println("Exception encountered. Terminating server.");
+			System.exit(0);
+		}
+    	System.out.println("ServerCom bound to the ID: " + Constant.SERVERCOM_ID + ".");
     	System.out.println("Server is running..");
     }
 }
