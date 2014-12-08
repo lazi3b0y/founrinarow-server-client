39ea32bb0dce196a35c172e5867c5896a40de58b
Simon <Simon@LZBY>
2014-12-08 21:12:20 +0100

    Pill pill.

--- FourInARowServer/src/com/server/exceptions/DrawnMoveException.java
+++ FourInARowServer/src/com/server/exceptions/DrawnMoveException.java
@@ -1,6 +1,8 @@
 package com.server.exceptions;
 
 public class DrawnMoveException extends Exception {
-    public DrawnMoveException() {
+	private static final long serialVersionUID = 5890148484913751682L;
+
+	public DrawnMoveException() {
     }
 }
