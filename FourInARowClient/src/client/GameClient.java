8fff46d866bda8931add5ccab96aede415759ef9
Simon <Simon@LZBY>
2014-12-08 22:02:41 +0100

    asd

--- FourInARowClient/src/client/GameClient.java
+++ FourInARowClient/src/client/GameClient.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -33,16 +34,48 @@ public class GameClient {
 			System.out.println("Exception encountered. Terminating client.");
 			System.exit(0);
 		}
-    	System.out.println("ClientCom instansiated.");
+    	System.out.println("ClientCom instansiated. Bindning now...");
     	try {
-			registry.rebind(Constant.CLIENTCOM1_ID, clientCom);
+			registry.bind(Constant.CLIENTCOM1_ID, clientCom);
+		} catch (AccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.toString(), "Exception Error.",JOptionPane.INFORMATION_MESSAGE);
+			System.out.println("Exception encountered, binding failed. Terminating client.");
+			System.exit(0);
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, e.toString(), "Exception Error.",JOptionPane.INFORMATION_MESSAGE);
-			System.out.println("Exception encountered. Terminating client.");
+			System.out.println("Exception encountered, binding failed. Terminating client.");
 			System.exit(0);
+		} catch (AlreadyBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, e.toString() + "\n\n" + Constant.CLIENTCOM1_ID + " is already bound to a different client. Trying to bind you to ClientCom2 instead.", "AlreadyBoundException",JOptionPane.INFORMATION_MESSAGE);
+			System.out.println("AlreadyBoundException encountered. Trying to bind to " + Constant.CLIENTCOM2_ID + ".");
+			try {
+				registry.bind(Constant.CLIENTCOM2_ID, clientCom);
+			} catch (AccessException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				JOptionPane.showMessageDialog(null, e.toString(), "Exception Error.",JOptionPane.INFORMATION_MESSAGE);
+				System.out.println("Exception encountered, binding failed. Terminating client.");
+				System.exit(0);
+			} catch (RemoteException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				JOptionPane.showMessageDialog(null, e.toString(), "Exception Error.",JOptionPane.INFORMATION_MESSAGE);
+				System.out.println("Exception encountered, binding failed. Terminating client.");
+				System.exit(0);
+			} catch (AlreadyBoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				JOptionPane.showMessageDialog(null, e.toString() + "\n\nOops, two players are already bound to the current server.", "AlreadyBoundException",JOptionPane.INFORMATION_MESSAGE);
+				System.out.println("AlreadyBoundException encountered twice, binding failed. Terminating client.");
+				System.exit(0);
+			}
 		}
-    	System.out.println("ClientCom bound to the ID: " + Constant.CLIENTCOM1_ID + ".");
+    	System.out.println("Binding successful!");
     }
 }
